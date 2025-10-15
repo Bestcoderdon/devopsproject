c7307b58099fd56b735e5fdbdac2a0341bf5dc1f
basilcloud <49113158+basilcloud@users.noreply.github.com>
2025-10-15 08:24:24 -0600

    Delete reports directory

--- reports/report-1.sql
+++
@@ -1,4 +0,0 @@
---All the countries in the world organised by largest population to smallest
-SELECT Name,  Population
-FROM Country
-ORDER BY Population DESC
--- reports/report-3.sql
+++
@@ -1,5 +0,0 @@
---All the countries in a region organised by largest population to smallest.
-SELECT Region,  SUM(Population) Population
-FROM Country
-GROUP BY Region
-ORDER BY Population DESC
